e3896127015df3636a0abd18a3a1c79206dec17f
xanderdecat <xander@decat.be>
2022-12-08 18:52:22 +0100

    update

--- src/main/java/com/example/group27/LogInUserController.java
+++ src/main/java/com/example/group27/LogInUserController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -22,10 +23,14 @@ public class LogInUserController {
     @FXML
     private TextField emailInput;
 
+    @FXML
+    private PasswordField passwordInput;
+
+
+
+
     // gaat verder naar HOMEPAGE
     public void goToHomePage(ActionEvent actionEvent) {
-
-
         if (!(emailInput.getText() == null || emailInput.getText().length() == 0)) {
             if (User.logUserInFromEmail(emailInput.toString())) {
                 try {
@@ -45,18 +50,18 @@ public class LogInUserController {
 
 
         // gaat terug naar WELCOMEPAGE
-        public void goToWelcomePage (ActionEvent actionEvent){
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("WelcomePage.fxml"));
-                Scene scene = new Scene(fxmlLoader.load(), 630, 400);
-                Stage stage = new Stage();
-                stage.setTitle("Event App");
-                stage.setScene(scene);
-                stage.show();
-                ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
-            } catch (IOException e) {
-            }
+    public void goToWelcomePage (ActionEvent actionEvent){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("WelcomePage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 630, 400);
+            Stage stage = new Stage();
+            stage.setTitle("Event App");
+            stage.setScene(scene);
+            stage.show();
+            ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
+        } catch (IOException e) {
         }
     }
+}
 
