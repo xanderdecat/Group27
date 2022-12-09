d037f683bcc3b84ecd36e82fd4cd9af45676efc7
xanderdecat <xander@decat.be>
2022-12-09 18:48:17 +0100

    update

--- src/main/java/com/example/group27/EventPageController.java
+++ src/main/java/com/example/group27/EventPageController.java
@@ -1,4 +1,11 @@
 package com.example.group27;
 
+import javafx.event.ActionEvent;
+
 public class EventPageController {
+    public void chooseArtist(ActionEvent actionEvent) {
+    }
+
+    public void goBack3(ActionEvent actionEvent) {
+    }
 }
