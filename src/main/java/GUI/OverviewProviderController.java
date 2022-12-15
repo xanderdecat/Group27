c9143caffc8d7f6570f8402ef2384f85e44f298a
xanderdecat <xander@decat.be>
2022-12-15 15:54:40 +0100

    update

--- src/main/java/GUI/OverviewProviderController.java
+++ src/main/java/GUI/OverviewProviderController.java
@@ -32,6 +32,8 @@ public class OverviewProviderController {
     private Label completeOverviewText;
     @FXML
     private Button seeAllButton;
+    @FXML
+    private Label enterValidDates;
 
     public void initialize() {
         int acceptedRequests = 0;
@@ -58,41 +60,46 @@ public class OverviewProviderController {
         charityToSet.setText(String.valueOf(Math.round((totalCharity) * 10) / 10.0));
         completeOverviewText.setVisible(false);
         seeAllButton.setVisible(false);
+        enterValidDates.setVisible(false);
     }
 
-    // nu kan je nog in de toekomst kijken
     public void seeSpecificOverview(ActionEvent actionEvent) {
         LocalDate startDateLD = startDatePicker.getValue();
         LocalDateTime startDate1 = LocalDateTime.of(startDateLD.getYear(), startDateLD.getMonth(), startDateLD.getDayOfMonth(), 00, 00, 00);
         LocalDate endDateLD = endDatePicker.getValue();
         LocalDateTime endDate1 = LocalDateTime.of(endDateLD.getYear(), endDateLD.getMonth(), endDateLD.getDayOfMonth(), 00, 00, 00);
 
-        int acceptedRequests = 0;
-        int declinedRequests = 0;
-        double totalEarned = 0;
-        double totalCharity = 0;
+        if (startDate1.isBefore(LocalDateTime.now()) && endDate1.isBefore(LocalDateTime.now())) {
+            enterValidDates.setVisible(false);
+            int acceptedRequests = 0;
+            int declinedRequests = 0;
+            double totalEarned = 0;
+            double totalCharity = 0;
 
-        for (Transaction transaction : TransactionDAO.getTransactions()) {
-            if (transaction.getProviderNumber() == Main.providerMain.getProviderNumber() && EventDAO.getEvent(transaction.getEventNumber()).getEndDate().isAfter(startDate1) && EventDAO.getEvent(transaction.getEventNumber()).getEndDate().isBefore(endDate1)) {
-                if (transaction.getStatus() == Transaction.status.Accepted )
-                    acceptedRequests = acceptedRequests + 1;
-                if (transaction.getStatus() == Transaction.status.Payed) {
-                    acceptedRequests = acceptedRequests + 1;
-                    totalEarned = totalEarned + transaction.getAmountToProvider();
-                    totalCharity = totalCharity + transaction.getAmountToNPO();
+            for (Transaction transaction : TransactionDAO.getTransactions()) {
+                if (transaction.getProviderNumber() == Main.providerMain.getProviderNumber() && EventDAO.getEvent(transaction.getEventNumber()).getEndDate().isAfter(startDate1) && EventDAO.getEvent(transaction.getEventNumber()).getEndDate().isBefore(endDate1)) {
+                    if (transaction.getStatus() == Transaction.status.Accepted)
+                        acceptedRequests = acceptedRequests + 1;
+                    if (transaction.getStatus() == Transaction.status.Payed) {
+                        acceptedRequests = acceptedRequests + 1;
+                        totalEarned = totalEarned + transaction.getAmountToProvider();
+                        totalCharity = totalCharity + transaction.getAmountToNPO();
+                    }
+                    if (transaction.getStatus() == Transaction.status.NotAccepted)
+                        declinedRequests = declinedRequests + 1;
                 }
-                if (transaction.getStatus() == Transaction.status.NotAccepted)
-                    declinedRequests = declinedRequests + 1;
             }
-        }
 
-        requestsToSet.setText(String.valueOf(acceptedRequests + declinedRequests));
-        acceptedToSet.setText(String.valueOf(acceptedRequests));
-        declinedToSet.setText(String.valueOf(declinedRequests));
-        earnedToSet.setText("€" + Math.round((totalEarned) * 10) / 10.0);
-        charityToSet.setText("€" + Math.round((totalCharity) * 10) / 10.0);
-        completeOverviewText.setVisible(true);
-        seeAllButton.setVisible(true);
+            requestsToSet.setText(String.valueOf(acceptedRequests + declinedRequests));
+            acceptedToSet.setText(String.valueOf(acceptedRequests));
+            declinedToSet.setText(String.valueOf(declinedRequests));
+            earnedToSet.setText("€" + Math.round((totalEarned) * 10) / 10.0);
+            charityToSet.setText("€" + Math.round((totalCharity) * 10) / 10.0);
+            completeOverviewText.setVisible(true);
+            seeAllButton.setVisible(true);
+        }
+        else
+            enterValidDates.setVisible(true);
     }
 
     public void seeAll(ActionEvent actionEvent) {
